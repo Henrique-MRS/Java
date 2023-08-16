00d828c6e3049bd99d12fa6da2b5f56670a2328d
Henrique Rocha <henrique_msouza@hotmail.com>
2023-08-16 19:08:46 -0300

    if and else exercise
    A instrução condicional if em Java destina-se a tomar uma decisão de acordo com o resultado de uma condição especificada (teste lógico). Dependendo do resultado retornado pelo teste lógico realizado, um determinado bloco de instruções será executado.
    *Excercício da faculdade de engenharia da computação*
    
    ¨The if conditional statement in Java is intended to make a decision according to the result of a specified condition (logical test). Depending on the result returned by the logical test performed, a specific block of instructions will be executed.¨
    *university practical exercise of computer engineering*

---
+++ IfandElse.java
@@ -0,0 +1,21 @@
+
+public class IfandElse {
+
+	public static void main(String[] args) {
+		
+		int valor = 9;
+				
+		if(valor==8) {
+			System.out.println("Valor = 8");
+		}
+		else 
+			if(valor > 8) {
+				System.out.println("Valor > 8");
+			}
+			else {
+				System.out.println("Valor < 8");
+				
+		}
+			
+	}
+}
